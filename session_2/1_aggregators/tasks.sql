4f878d5f29fe680b4998cd23e07d5986f978328c
Oli Cruise <cruiseoli@outlook.com>
2026-01-30 10:51:23 +0000

    End of session 2

--- session_2/1_aggregators/tasks.sql
+++ session_2/1_aggregators/tasks.sql
@@ -7,3 +7,9 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+-- 1) SELECT Continent, AVG(Population) FROM countries GROUP BY Continent;
+-- 2) SELECT Continent, SUM(Population) FROM countries GROUP BY Continent;
+-- 3) SELECT Continent, MAX(Population) FROM countries GROUP BY Continent;
+-- 4) SELECT Continent, Country, MIN(AreaSqMi) FROM countries GROUP BY Continent;
+-- 5) SELECT Continent, COUNT(Country) FROM countries GROUP BY Continent;
+-- 6) SELECT Continent, AVG(GDPPerCapita) FROM countries GROUP BY Continent ORDER BY AVG(GDPPerCapita) ASC;
